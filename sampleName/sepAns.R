948e1fe3f16394ddb5a374043134d0085e5e4f31
Xiaoyang <xd14188@bristol.ac.uk>
2025-07-21 16:35:28 +0100

    sample duplicated Pops

--- sampleName/sepAns.R
+++ sampleName/sepAns.R
@@ -6,6 +6,15 @@ dat[which(dat$Superpopulation.code == "EUR,AFR"), ]
 
 data <- dat[-which(dat$Superpopulation.code == "EUR,AFR"), ]
 
+# > print(unique(dat$Superpopulation.code))
+# [1] "EUR"     "EAS"     "AMR"     "AFR"     "SAS"     "EUR,AFR"
+# > 
+#   > dat[which(dat$Superpopulation.code == "EUR,AFR"), ]
+# Sample.name  Sex Biosample.ID Population.code Population.name Superpopulation.code               Superpopulation.name
+# 1785     HG01783 male   SAME124427         IBS,MSL   Iberian,Mende              EUR,AFR European Ancestry,African Ancestry
+# Population.elastic.ID                                                               Data.collections
+# 1785               IBS,MSL 1000 Genomes on GRCh38,1000 Genomes 30x on GRCh38,1000 Genomes phase 3 release
+
 print(unique(data$Superpopulation.code))
 pops <- unique(data$Superpopulation.code)
 
